d270ba225ccd12c585febabf84c55169861ca48d
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-08 06:21:09 +0300

    create attributes for user product and review

--- db/migrate/20221208031747_create_products.rb
+++ db/migrate/20221208031747_create_products.rb
@@ -1,6 +1,12 @@
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
+      t.string :title
+      t.string :description
+      t.integer :price
+      t.string :image_url
+      t.integer :quantity
+      t.string :category
 
       t.timestamps
     end
--- db/migrate/20221208031808_create_reviews.rb
+++ db/migrate/20221208031808_create_reviews.rb
@@ -1,7 +1,11 @@
 class CreateReviews < ActiveRecord::Migration[7.0]
   def change
     create_table :reviews do |t|
-
+      t.integer :rating
+      t.string :comment
+      t.belongs_to :user
+      t.belongs_to :product
+      
       t.timestamps
     end
   end
