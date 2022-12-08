fdfc6eb90455d8800f13bcb203ce0ef921a7ee48
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-08 06:34:13 +0300

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :products
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "products#index"
+  resources :products, only: [:index, :show, :create, :update, :destroy]
+  resources :reviews, only: [:index, :show, :create, :destroy]
+
+  # get users
+  get '/users', to: 'users#index'
+
+  # Authentication
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  # Creating Account && staying authenticated
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+
 end
